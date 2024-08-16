023ec8dd9f2377979992995f48492b1d9ba8a94f
usman64 <usmantariq65@outlook.com>
2024-08-16 21:02:27 +0500

    Fix test

--- 3. Integration and E2E Testing/1. Integration Testing/scripts/run-integration.sh
+++ 3. Integration and E2E Testing/1. Integration Testing/scripts/run-integration.sh
@@ -3,4 +3,4 @@ echo '🟡 - Waiting for database to be ready...'
 ./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres" -- echo '🟢 - Database is ready!'
 npx prisma migrate dev --name init
 npm run test
-docker-compose down
+docker compose down
